f3251453686be0d938c8bb40441c4ab00c09689c
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2023-09-13 01:32:54 +0300

    Создание слайда

--- build/js/main.js
+++ build/js/main.js
@@ -60,3 +60,10 @@ function init() {
   clusterer.add(geoObjects)
 }
 
+// Slider
+
+const container = document.querySelector('.slider');
+document.querySelector('.slider__input').addEventListener('input', (e) => {
+  container.style.setProperty('--position', `${e.target.value}%`);
+})
+
