4c54766297573a20a1ad3ffc2d414eb3140ba487
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2023-09-18 22:37:43 +0300

    исправляет ошибки

--- source/js/main.js
+++ source/js/main.js
@@ -66,4 +66,3 @@ const container = document.querySelector('.slider');
 document.querySelector('.slider__input').addEventListener('input', (e) => {
   container.style.setProperty('--position', `${e.target.value}%`);
 })
-
